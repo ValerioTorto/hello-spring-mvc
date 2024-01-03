42bb686981c12093e83f6c680d0e89fe9f67a6ee
ValerioTorto <valerio.tortorelli@gmail.com>
2024-01-03 16:15:50 +0100

    first exercise done

---
+++ src/main/java/org/learning/hellospringmvc/controller/IndexController.java
@@ -0,0 +1,23 @@
+package org.learning.hellospringmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Random;
+
+@Controller
+@RequestMapping("/")
+
+public class IndexController {
+
+
+    @GetMapping
+    public String index(Model model){
+        Random random = new Random();
+        model.addAttribute("random",random.nextInt(1,6));
+        return "home";
+    }
+}
